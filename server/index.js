df6f85db254b13f615305e55c67a7ab1ca82d755
Ci3t <ranigfx@gmail.com>
2023-01-22 14:17:25 +0200

    final

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ import connectDB from './mongodb/connect.js';
 
 import  dalleRoutes  from './routes/dalle.routes.js';
 import postRoutes  from './routes/post.routes.js';
-
+import * as url from 'url';
+import path from 'path';
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
 
 dotenv.config();
 
@@ -15,8 +17,16 @@ app.use(cors());
 const PORT = 8080 || process.env.PORT
 app.use(express.json({limit:'50mb'}));
 
+
+const publicPath = path.join(__dirname, 'build');
+app.use(express.static(publicPath));
+
 app.use('/api/v1/post',postRoutes);
 app.use('/api/v1/dalle',dalleRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
 
 app.get('/',async(req,res)=>{
     res.send('Hello DALL-E!')
