b2b96005ed06dfdb5da069a774e468f6d9f4fb49
Ci3t <ranigfx@gmail.com>
2023-01-22 16:42:43 +0200

    final s

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ import connectDB from './mongodb/connect.js';
 
 import  dalleRoutes  from './routes/dalle.routes.js';
 import postRoutes  from './routes/post.routes.js';
-import * as url from 'url';
-import path from 'path';
-const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
+// import * as url from 'url';
+// import path from 'path';
+// const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
 
 dotenv.config();
 
@@ -18,15 +18,15 @@ const PORT = 8080 || process.env.PORT
 app.use(express.json({limit:'50mb'}));
 
 
-const publicPath = path.join(__dirname, 'client','dist');
-app.use(express.static(publicPath));
+// const publicPath = path.join(__dirname, 'client','dist');
+// app.use(express.static(publicPath));
 
 app.use('/api/v1/post',postRoutes);
 app.use('/api/v1/dalle',dalleRoutes);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath,'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath,'index.html'));
+// });
 
 
 app.get('/',async(req,res)=>{
@@ -37,7 +37,7 @@ app.get('/',async(req,res)=>{
 const startServer = async ()=>{
 
     try{
-        connectDB(process.env.MONGODB_URL);
+         connectDB(process.env.MONGODB_URL);
         app.listen(PORT,()=>{
             console.log(`Server has Started on ${PORT}` )
         })
